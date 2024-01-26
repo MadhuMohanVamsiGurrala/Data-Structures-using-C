b28c97168020ddeeda3059f3fd919e4c89ba1ca2
MadhuMohanVamsiGurrala <139125188+MadhuMohanVamsiGurrala@users.noreply.github.com>
2024-01-26 11:56:35 +0530

    Add files via upload

---
+++ EvenSum.c
@@ -0,0 +1,13 @@
+/*A program in C to display the n terms of even natural number and their sum*/
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter a number=");
+scanf("%d",&n);
+if(n%2==0)
+printf("Number is even");
+else
+printf("Number is odd");
+}
